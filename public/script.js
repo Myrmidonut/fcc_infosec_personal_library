d13f521b8b2c91cc5bc008801f5615c1962557fb
Glitch (fcc-infosec-personal-library-fred) <none>
2018-11-13 18:50:36 +0000

    frontend rework, no more jquery, tests pass

---
+++ public/script.js
@@ -0,0 +1,146 @@
+const booksList = document.getElementById("booksList");
+const commentsList = document.getElementById("commentsList");
+const bookTitle = document.getElementById("bookTitle");
+const bookInterface = document.getElementById("bookInterface");
+const newBookForm = document.getElementById("newBookForm");
+const deleteAllBooksButton = document.getElementById("deleteAllBooks");
+
+let dataPosition;
+
+getBooks();
+
+newBookForm.addEventListener("submit", e => {
+  e.preventDefault();
+  
+  const url = "/api/books";
+  
+  fetch(url, {
+    method: "post",
+    body: new URLSearchParams(new FormData(newBookForm))
+  })
+  .then(response => response.json)
+  .then(data => {
+    getBooks();
+    newBookForm.reset();
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+deleteAllBooksButton.addEventListener("click", e => {
+  e.preventDefault();
+  
+  const url = "/api/books";
+  
+  fetch(url, {
+    method: "delete"
+  })
+  .then(response => response.text())
+  .then(data => {
+    getBooks();
+    clearBookDetails();
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+function showBooksList(data) {
+  booksList.innerHTML = "";
+  
+  let books = '<ul>';
+  
+  data.forEach((e, i) => {
+    books += `<li class="bookItem" id="${e._id} ${i}">${e.title} - ${e.comments.length} comments</li>`
+  })
+  books += "</ul>";
+  booksList.innerHTML = books;
+}
+
+function clearBookDetails() {
+  bookTitle.innerHTML = "";
+  commentsList.innerHTML = "";
+  bookInterface.innerHTML = "";
+}
+
+function deleteBook(data, dataPosition) {
+  const url = `/api/books/${data[dataPosition]._id}`;
+        
+  fetch(url, {
+    method: "delete"
+  })
+  .then(response => response.text())
+  .then(data => {
+    getBooks();
+    clearBookDetails();
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
+
+function showBookInterface(data, dataPosition) {
+  bookInterface.innerHTML += 
+    `<form id="addCommentForm">
+      <input type="text" name="comment" placeholder="Comment" required="">
+      <input type="submit" value="Add comment">
+    </form>
+    <button id="deleteBook">Delete Book</button>`
+  
+  const addCommentForm = document.getElementById("addCommentForm");
+  
+  addCommentForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    fetch(`/api/books/${data[dataPosition]._id}`, {
+      method: 'post',
+      body: new URLSearchParams(new FormData(addCommentForm))
+    })
+    .then(response => response.json())
+    .then((data) => {
+      commentsList.innerHTML += `<li>${data.comments[data.comments.length - 1]}</li>`
+      addCommentForm.reset();
+      getBooks();
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  })
+  
+  const deleteBookButton = document.getElementById("deleteBook")
+  
+  deleteBookButton.addEventListener("click", () => deleteBook(data, dataPosition))
+}
+
+function showBookDetails(bookItem, data) {
+  bookItem.forEach(e => {
+    e.addEventListener("click", () => {
+      clearBookDetails();
+      
+      dataPosition = e.id.split(" ")[1]
+      
+      bookTitle.innerHTML = `Title: ${data[dataPosition].title}`
+      data[dataPosition].comments.forEach((e, i) => {
+        commentsList.innerHTML += `<li>${e}</li>`
+      })
+      
+      showBookInterface(data, dataPosition)
+    })
+  })
+}
+
+function getBooks() {
+  fetch("/api/books")
+  .then(response => response.json())
+  .then(data => {
+    showBooksList(data)
+
+    const bookItem = document.querySelectorAll(".bookItem");
+
+    showBookDetails(bookItem, data)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
--- routes/api.js
+++ routes/api.js
@@ -9,33 +9,28 @@ const MONGODB_CONNECTION_STRING = process.env.DB;
 module.exports = app => {
   app.route('/api/books')
     .get((req, res) => {
-      // response will be array of book objects
-      // json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-    
       MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
         const collection = db.collection("books");
         
         collection.find().toArray((err, result) => {
-          res.json(result)
+          res.json(result);
         })
       })
     })
     
     .post((req, res) => {
-      // response will contain new book object including atleast _id and title
-    
       const title = req.body.title;
-      const commentcount = 0;
     
-      if (!req.body.title) res.send("missing title");
-      else {
+      if (!req.body.title) {
+        res.send("missing title");
+      } else {
         MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
           const collection = db.collection("books");
 
           collection.insert({
             title: title,
-            commentcount: commentcount,
-            comments: []
+            comments: [],
+            commentcount: 0
           }, (err, result) => {
             res.json(result.ops[0]);
           })
@@ -44,8 +39,6 @@ module.exports = app => {
     })
     
     .delete((req, res) => {
-      // if successful response will be 'complete delete successful'
-      
       MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
         const collection = db.collection("books");
         
@@ -57,57 +50,49 @@ module.exports = app => {
 
   app.route('/api/books/:id')
     .get((req, res) => {
-      // json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}, empty comments array if not present
-    
       const bookid = req.params.id;
     
       MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
         const collection = db.collection("books");
         
         collection.find({_id: ObjectId(bookid)}).toArray( (err, result) => {
-          if (!result[0]) res.send("no book exists");
-          else if (!result[0].comments) res.json({
-            _id: result[0]._id,
-            title: result[0].title,
-            comments: []
-          });
-          else res.json({
-            _id: result[0]._id,
-            title: result[0].title,
-            comments: result[0].comments
-          });
+          if (!result[0]) {
+            res.send("no book exists");
+          } else {
+            res.json({
+              _id: result[0]._id,
+              title: result[0].title,
+              comments: result[0].comments
+            });
+          }
         })
       })
-      
     })
     
-    .post(function(req, res){
-      // json res format same as .get
-    
+    .post((req, res) => {
       const bookid = req.params.id;
       const comment = req.body.comment;
     
       MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
         const collection = db.collection("books");
         
-        collection.update({_id: ObjectId(bookid)}, {$push: {comments: comment}}, (err, result) => {
-
-        })
+        collection.update({_id: ObjectId(bookid)}, {$push: {comments: comment}}, (err, result) => {})
         
         collection.find({_id: ObjectId(bookid)}).toArray( (err, result) => {
-          if (!result[0]) res.send("no book exists");
-          else res.json({
-            _id: result[0]._id,
-            title: result[0].title,
-            comments: result[0].comments
-          });
+          if (!result[0]) {
+            res.send("no book exists");
+          } else {
+            res.json({
+              _id: result[0]._id,
+              title: result[0].title,
+              comments: result[0].comments
+            });
+          }
         })
       })
     })
     
-    .delete(function(req, res){
-      // if successful response will be 'delete successful'
-    
+    .delete((req, res) => {
       const bookid = req.params.id;
       
       MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
